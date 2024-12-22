b06c0c658d0ea462841f8bc57420d7f1b0dae609
caichuanwang <cw.cai@yamu.com>
2024-12-22 22:04:32 +0800

    feat: 217

--- 217.存在重复元素.py
+++ 217.存在重复元素.py
@@ -6,7 +6,21 @@
 
 # @lc code=start
 class Solution:
-    def containsDuplicate(self, nums: List[int]) -> bool:
-        
+    def containsDuplicate(self, nums: list[int]) -> bool:
+        d = dict()
+        for v in nums:
+            if v in d:
+                return True
+            else:
+                d[v] = None
+        return False
+
+
+    def containsDuplicate(self, nums: list[int]) -> bool:
+        s = set(nums)
+        return len(s) != len(nums)
+
+s = Solution()
+print(s.containsDuplicate([1,2,3,4]))
 # @lc code=end
 
